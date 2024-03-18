efec1535a00fe56e4988ab9fc1c11519ed9e95c4
junkdeveloper <q906107506@gmail.com>
2024-03-17 22:53:00 -0500

    Update react-router-dom imports

--- src/App.js
+++ src/App.js
@@ -3,25 +3,23 @@ import Nav from "./components/Nav";
 import Footer from "./components/Footer";
 import Homepage from "./pages/Homepage";
 import About from "./pages/About";
-import { Routes, Route, HashRouter } from "react-router-dom"; // 更新引入
+import { Routes, Route } from "react-router-dom"; // 更新引入
 import "./styles/style.css";
 
 function App() {
   return (
-    <HashRouter basename="/">
-      <div className="App">
-        <Nav />
-        <Routes>
-          {" "}
-          {/* 使用 Routes 代替 Switch */}
-          <Route path="/" element={<Homepage />} exact />{" "}
-          {/* 更新 Route 的用法 */}
-          <Route path="/about" element={<About />} exact />{" "}
-          {/* 更新 Route 的用法 */}
-        </Routes>
-        <Footer />
-      </div>
-    </HashRouter>
+    <div className="App">
+      <Nav />
+      <Routes>
+        {" "}
+        {/* 使用 Routes 代替 Switch */}
+        <Route path="/" element={<Homepage />} exact />{" "}
+        {/* 更新 Route 的用法 */}
+        <Route path="/about" element={<About />} exact />{" "}
+        {/* 更新 Route 的用法 */}
+      </Routes>
+      <Footer />
+    </div>
   );
 }
 
